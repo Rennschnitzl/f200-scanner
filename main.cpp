87d160a3e1b29a875f65249c15291320630c525a
Rennschnitzl <pitter.flock@googlemail.com>
2016-01-20 17:57:56 +0100

    adding code cleanup some pcl alogorithms

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/surface/mls.h>
+#include <pcl/registration/icp.h>
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/surface/gp3.h>
 #include <pcl/io/vtk_io.h>
+#include <pcl/point_types_conversion.h>
+#include <pcl/surface/impl/mls.hpp> // MSL for PointXYZINormals
 
 
 #define RECORD_STACK_SIZE 40
@@ -63,54 +67,15 @@ void display(Frame frame1)
     cv::imshow("belief", frame1.belief);
 }
 
-int main()
+void triangulation(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered)
 {
-    CameraWrapper cw(0);
-    Frame frame1(IMAGE_WIDTH, IMAGE_HEIGHT, cw.getCameraMatrix(),cw.getCoeffs());
 
-    // RECORD & PROCESS
-    cw.recordStack(RECORD_STACK_SIZE,frame1.rawStackIR, frame1.rawStackDepth);
-    Converter::analyseStack(frame1.rawStackDepth, frame1.belief, frame1.processedImageDepth);
-    Converter::averageIR(frame1.rawStackIR, frame1.processedImageIR);
-    Converter::undistortDepth(frame1.processedImageDepth, frame1.cameraMatrix, frame1.coefficients, frame1.belief);
+    // convert XYZI to normal cloud
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_convert (new pcl::PointCloud <pcl::PointXYZ>);
+    //pcl::PointXYZINormal
 
-    // CONVERT TO CLOUD
-    pcl::PointCloud<pcl::PointXYZI> cloud;
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudptr;
-    cloudptr = cloud.makeShared();
-    Converter::depthTo3d(frame1.processedImageDepth,cw.getCameraMatrix(),cloudptr, frame1.belief);
-
-    // DISPLAY
-    display(frame1);
-
-    // PCL VIEWER
-    Eigen::Affine3f transform = Eigen::Affine3f::Identity();
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-    viewer->registerPointPickingCallback(pp_callback, (void*)&viewer);
-    viewer->setBackgroundColor (0, 0, 0);
-    viewer->initCameraParameters ();
-    viewer->addCoordinateSystem(1.0, "Origin");
-
-    // filter cloud
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZI>);
-//    pcl::RadiusOutlierRemoval<pcl::PointXYZI> outrem;
-//    // build the filter
-//    outrem.setInputCloud(cloudptr);
-//    outrem.setRadiusSearch(0.15);
-//    outrem.setMinNeighborsInRadius (5);
-//    // apply filter
-//    outrem.filter (*cloud_filtered);
-    pcl::StatisticalOutlierRemoval<pcl::PointXYZI> sor;
-    sor.setInputCloud (cloudptr);
-    sor.setMeanK (50);
-    sor.setStddevMulThresh (1.0);
-    sor.filter (*cloud_filtered);
-    std::cout << "filter removed " << (cloudptr->size() - cloud_filtered->size()) << " points" << std::endl;
-
-    /// **************
-    /// TRIANGULATION
-    /// **************
     // Normal estimation*
+
     pcl::NormalEstimation<pcl::PointXYZI, pcl::Normal> n;
     pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
     pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI>);
@@ -121,21 +86,23 @@ int main()
     n.compute (*normals);
     //* normals should not contain the point normals + surface curvatures
 
+
+
     // Concatenate the XYZ and normal fields*
-    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZINormal>);
     pcl::concatenateFields (*cloud_filtered, *normals, *cloud_with_normals);
     //* cloud_with_normals = cloud + normals
 
     // Create search tree*
-    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+    pcl::search::KdTree<pcl::PointXYZINormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZINormal>);
     tree2->setInputCloud (cloud_with_normals);
 
     // Initialize objects
-    pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+    pcl::GreedyProjectionTriangulation<pcl::PointXYZINormal> gp3;
     pcl::PolygonMesh triangles;
 
     // Set the maximum distance between connected points (maximum edge length)
-    gp3.setSearchRadius (0.025);
+    gp3.setSearchRadius (0.15);
 
     // Set typical values for the parameters
     gp3.setMu (2.5);
@@ -155,14 +122,146 @@ int main()
     // Additional vertex information
     std::vector<int> parts = gp3.getPartIDs();
     std::vector<int> states = gp3.getPointStates();
+}
+
+void interpolate(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered, pcl::PointCloud<pcl::PointXYZINormal> mls_points)
+{
+    // Create a KD-Tree
+    pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI>);
+
+
+    // Init object (second point type is for the normals, even if unused)
+    pcl::MovingLeastSquares<pcl::PointXYZI, pcl::PointXYZINormal> mls;
+
+    mls.setComputeNormals (true);
+
+    // Set parameters
+    mls.setInputCloud (cloud_filtered);
+    mls.setPolynomialFit (true);
+    mls.setSearchMethod (tree);
+    mls.setSearchRadius (0.02);
+
+    // Reconstruct
+    mls.process (mls_points);
+}
+
+void statistical(pcl::PointCloud<pcl::PointXYZI>::Ptr cloudptr, pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered)
+{
+    //    pcl::RadiusOutlierRemoval<pcl::PointXYZI> outrem;
+    //    // build the filter
+    //    outrem.setInputCloud(cloudptr);
+    //    outrem.setRadiusSearch(0.15);
+    //    outrem.setMinNeighborsInRadius (5);
+    //    // apply filter
+    //    outrem.filter (*cloud_filtered);
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZI> sor;
+    sor.setInputCloud (cloudptr);
+    sor.setMeanK (50);
+    sor.setStddevMulThresh (1.0);
+    sor.filter (*cloud_filtered);
+    std::cout << "filter removed " << (cloudptr->size() - cloud_filtered->size()) << " points" << std::endl;
+}
+
+int main()
+{
+    CameraWrapper cw(0);
+
+    std::cout << "recording first frame" << std::endl;
+
+    Frame frame1(IMAGE_WIDTH, IMAGE_HEIGHT, cw.getCameraMatrix(),cw.getCoeffs());
+
+    // RECORD & PROCESS
+    cw.recordStack(RECORD_STACK_SIZE,frame1.rawStackIR, frame1.rawStackDepth);
+    Converter::analyseStack(frame1.rawStackDepth, frame1.belief, frame1.processedImageDepth);
+    Converter::averageIR(frame1.rawStackIR, frame1.processedImageIR);
+    Converter::undistortDepth(frame1.processedImageDepth, frame1.cameraMatrix, frame1.coefficients, frame1.belief);
+
+    // CONVERT TO CLOUD
+    pcl::PointCloud<pcl::PointXYZI> cloud;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudptr;
+    cloudptr = cloud.makeShared();
+    Converter::depthTo3d(frame1.processedImageDepth,cw.getCameraMatrix(),cloudptr, frame1.belief);
+
+    // DISPLAY
+    display(frame1);
+
+    std::cout << "waiting for second frame" << std::endl;
+    cv::waitKey(0);
+
+    Frame frame2(IMAGE_WIDTH, IMAGE_HEIGHT, cw.getCameraMatrix(),cw.getCoeffs());
+
+    // RECORD & PROCESS
+    cw.recordStack(RECORD_STACK_SIZE,frame2.rawStackIR, frame2.rawStackDepth);
+    Converter::analyseStack(frame2.rawStackDepth, frame2.belief, frame2.processedImageDepth);
+    Converter::averageIR(frame2.rawStackIR, frame2.processedImageIR);
+    Converter::undistortDepth(frame2.processedImageDepth, frame2.cameraMatrix, frame2.coefficients, frame2.belief);
+
+    // CONVERT TO CLOUD
+    pcl::PointCloud<pcl::PointXYZI> cloud2;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudptr2;
+    cloudptr2 = cloud2.makeShared();
+    Converter::depthTo3d(frame2.processedImageDepth,cw.getCameraMatrix(),cloudptr2, frame2.belief);
+
+    // DISPLAY
+    display(frame2);
+
+    cv::waitKey(0);
 
     /// **************
-    /// /TRIANGULATION
+    /// Init viewer
     /// **************
+    Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->registerPointPickingCallback(pp_callback, (void*)&viewer);
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->initCameraParameters ();
+    viewer->addCoordinateSystem(1.0, "Origin");
 
+    /// **************
+    /// Statistical outlier removal
+    /// **************
+//    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZI>);
+//    statistical(cloudptr, cloud_filtered);
+
+    /// **************
+    /// Triangulation
+    /// writes vtk mesh to disk
+    /// **************
+    //triangulation(cloud_filtered);
+
+
+    /// **************
+    /// Interpolation
+    /// **************
+//    pcl::PointCloud<pcl::PointXYZINormal> mls_points;
+//    pcl::PointCloud<pcl::PointXYZINormal>::Ptr mls_ptr = mls_points.makeShared();
+
+//    interpolate(cloud_filtered, mls_points);
+//    viewer->addPointCloud<pcl::PointXYZINormal>(mls_ptr, "frame");
+
+    /// **************
+    /// ICP
+    /// **************
+    pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
+    icp.setInputSource(cloudptr);
+    //icp.setInputCloud(cloudptr);
+    icp.setInputTarget(cloudptr2);
+    pcl::PointCloud<pcl::PointXYZI> Final;
+    icp.align(Final);
+    std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+                 icp.getFitnessScore() << std::endl;
+    std::cout << icp.getFinalTransformation() << std::endl;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr Finalptr = Final.makeShared();
+
+
+    /// **************
+    /// Display in Viewer
+    /// **************
     // add point cloud
-    pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity_distribution(cloud_filtered, "intensity");
-    viewer->addPointCloud<pcl::PointXYZI>(cloud_filtered,intensity_distribution,"frame");
+//    pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity_distribution(cloudptr, "intensity");
+//    viewer->addPointCloud<pcl::PointXYZI>(cloudptr,intensity_distribution,"framei");
+    viewer->addPointCloud<pcl::PointXYZI>(cloudptr2,"target");
+    viewer->addPointCloud<pcl::PointXYZI>(Finalptr,"frame");
 
     cv::waitKey(0);
 
